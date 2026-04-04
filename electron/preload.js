01d9e04eae23052c1aa99677634f903cf2a6147e
thundergod <s6dative@gmail.com>
2026-04-04 16:39:56 -0400

    fix: preload vusd-balance -> vusd-balance-parsed, add all missing handlers

--- electron/preload.js
+++ electron/preload.js
@@ -1,10 +1,13 @@
-const { contextBridge, ipcRenderer } = require('electron')
-contextBridge.exposeInMainWorld('electron', {
-  vusd:        (args)    => ipcRenderer.invoke('vusd', args),
-  bitcoinCli:  (args)    => ipcRenderer.invoke('bitcoin-cli', args),
-  faucet:      (address) => ipcRenderer.invoke('faucet', address),
-  btcBalance:  ()        => ipcRenderer.invoke('btc-balance'),
-  btcAddress:  ()        => ipcRenderer.invoke('btc-address'),
-  readVaults:  ()        => ipcRenderer.invoke('read-vaults'),
-  vusdBalance: ()        => ipcRenderer.invoke('vusd-balance'),
+const { contextBridge, ipcRenderer, shell } = require("electron")
+contextBridge.exposeInMainWorld("electron", {
+  vusd:        (args)    => ipcRenderer.invoke("vusd", args),
+  bitcoinCli:  (args)    => ipcRenderer.invoke("bitcoin-cli", args),
+  faucet:      (address) => ipcRenderer.invoke("faucet", address),
+  btcBalance:  ()        => ipcRenderer.invoke("btc-balance"),
+  btcAddress:  ()        => ipcRenderer.invoke("btc-address"),
+  readVaults:  ()        => ipcRenderer.invoke("read-vaults"),
+  vusdBalance: ()        => ipcRenderer.invoke("vusd-balance-parsed"),
+  vusdOracle:  ()        => ipcRenderer.invoke("vusd-oracle-parsed"),
+  nodeInfo:    ()        => ipcRenderer.invoke("node-info"),
+  shell: { openExternal: (url) => shell.openExternal(url) },
 })
